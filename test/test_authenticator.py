a3c2ac1ee52d864bdac257a8c795d81b6156891c
Ramon Bouckaert <ramon.bouckaert@zepben.com>
2021-08-19 03:22:11 +1000

    Fix failing tests
    Signed-off-by: Ramon Bouckaert <ramon.bouckaert@zepben.com>

--- test/test_authenticator.py
+++ test/test_authenticator.py
@@ -11,7 +11,7 @@ from unittest.mock import ANY
 
 import pytest
 
-from zepben.auth.authenticator import ZepbenAuthenticator, AuthException, create_authenticator
+from zepben.auth.authenticator import ZepbenAuthenticator, AuthException, create_authenticator, AuthMethod
 
 TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZha2VraWQifQ.eyJpc3MiOiJodHRwczovL2lzc3Vlci8iLCJzdWIiOiJmYWtlIiwiYXVkIjoiaHR0cHM6Ly9mYWtlLWF1ZC8iLCJpYXQiOjE1OTE4MzQxNzksImV4cCI6OTU5MTkyMDU3OSwiYXpwIjoid2U5ZDNSME5jTUNWckpDZ2ROSWVmWWx6aHo2VE9SaGciLCJzY29wZSI6IndyaXRlOm5ldHdvcmsgcmVhZDpuZXR3b3JrIHdyaXRlOm1ldHJpY3MgcmVhZDpld2IiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJwZXJtaXNzaW9ucyI6WyJ3cml0ZTpuZXR3b3JrIiwicmVhZDpuZXR3b3JrIiwid3JpdGU6bWV0cmljcyIsInJlYWQ6ZXdiIl19.ay_YTwRsfcNzVdmQ4EgmuNMMypfZIIc8K9dCCtLqUmUJDtE7NUuKaVAmGDdmW1J-ngm0UsH4k6B5QpPIJnLIROpdDf7aRzdE9hNFuSHR3arpyCzmO2-TiFDZLFXQjHf0Q-BaxGoXLQBupGYuQaG_3flaLPB3hPV0nqPoBTIoJgG8n2w0Uo2tePe_y2Blqco1sK2wElwyMlYc-UuTyFSvwKlpSXYmO4ppVmbAa9lS2ley6lcv2TwXLCk0KfIIH2E5OBvJHevZqYEzFBAeLCnahKoWxexsVvEfZr40Nhc6oPRT5yJfHRBnCrDnO1fE96rqguQpsDG-HWCtd2GkpnAXNg"
 
@@ -20,7 +20,8 @@ mock_auth0_issuer_domain = ''.join(random.choices(string.ascii_lowercase, k=10))
 mock_access_token = ''.join(random.choices(string.ascii_lowercase, k=10))
 mock_refresh_token = ''.join(random.choices(string.ascii_lowercase, k=10))
 mock_issuer_protocol = ''.join(random.choices(string.ascii_lowercase, k=10))
-
+mock_auth_method = random.choice(list(AuthMethod))
+mock_verify_certificate = bool(random.getrandbits(1))
 
 class MockResponse:
     def __init__(self, json_data, status_code, reason="", text=""):
@@ -89,8 +90,14 @@ class TestZepbenAuthenticator:
     @mock.patch('zepben.auth.authenticator.requests.post', side_effect=lambda *args, **kwargs: MockResponse(
         {"access_token": TOKEN, "refresh_token": mock_refresh_token, "token_type": "Bearer"}, 200))
     def test_fetch_token_successful(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
 
@@ -99,13 +106,20 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.token_request_data
+            data=authenticator.token_request_data,
+            verify=mock_verify_certificate
         )  # Appropriate-looking password grant request was made to the issuer
 
     @mock.patch('zepben.auth.authenticator.requests.post', side_effect=lambda *args, **kwargs: MockResponse(None, 404, "test reason", "test text"))
     def test_fetch_token_throws_exception_on_bad_response(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
 
@@ -115,13 +129,20 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.token_request_data
+            data=authenticator.token_request_data,
+            verify=mock_verify_certificate
         )
 
     @mock.patch('zepben.auth.authenticator.requests.post', side_effect=lambda *args, **kwargs: MockResponse(None, 200, "test reason", "test text"))
     def test_fetch_token_throws_exception_on_missing_json(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
 
@@ -131,14 +152,21 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.token_request_data
+            data=authenticator.token_request_data,
+            verify=mock_verify_certificate
         )
 
     @mock.patch('zepben.auth.authenticator.requests.post',
                 side_effect=lambda *args, **kwargs: MockResponse({'error': 'fail', 'error_description': 'test error description'}, 200))
     def test_fetch_token_throws_exception_on_error_response(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
 
@@ -148,14 +176,21 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.token_request_data
+            data=authenticator.token_request_data,
+            verify=mock_verify_certificate
         )
 
     @mock.patch('zepben.auth.authenticator.requests.post',
                 side_effect=lambda *args, **kwargs: MockResponse({'test': 'fail'}, 200))
     def test_fetch_token_throws_exception_on_missing_access_token(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
 
@@ -165,14 +200,21 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.token_request_data
+            data=authenticator.token_request_data,
+            verify=mock_verify_certificate
         )
 
     @mock.patch('zepben.auth.authenticator.requests.post', side_effect=lambda *args, **kwargs: MockResponse(
         {"access_token": TOKEN, "refresh_token": mock_refresh_token, "token_type": "Bearer"}, 200))
     def test_fetch_token_successful_using_refresh(self, mock_post):
-        authenticator = ZepbenAuthenticator(audience=mock_audience, issuer_domain=mock_auth0_issuer_domain, issuer_protocol=mock_issuer_protocol,
-                                            token_path="/fake/path")
+        authenticator = ZepbenAuthenticator(
+            audience=mock_audience,
+            issuer_domain=mock_auth0_issuer_domain,
+            auth_method=mock_auth_method,
+            verify_certificate=mock_verify_certificate,
+            issuer_protocol=mock_issuer_protocol,
+            token_path="/fake/path"
+        )
 
         authenticator.refresh_request_data['refresh_token'] = mock_refresh_token
         mock_post.assert_not_called()  # POST request is not made before get_token() is called
@@ -182,5 +224,6 @@ class TestZepbenAuthenticator:
         mock_post.assert_called_once_with(
             f"{mock_issuer_protocol}://{mock_auth0_issuer_domain}/fake/path",
             headers=ANY,
-            data=authenticator.refresh_request_data
+            data=authenticator.refresh_request_data,
+            verify=mock_verify_certificate
         )
