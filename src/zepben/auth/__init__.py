8485afa9edcd051ab1f937f9dbd034e91832acaa
Marcus Koh <marcuskoh29@gmail.com>
2022-02-04 12:57:47 +1100

    Fix subpackage __init__
    Signed-off-by: Marcus Koh <marcuskoh29@gmail.com>

--- src/zepben/auth/__init__.py
+++ src/zepben/auth/__init__.py
@@ -4,4 +4,3 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from zepben.auth.client.token_fetcher import ZepbenTokenFetcher, AuthMethod, create_token_fetcher
